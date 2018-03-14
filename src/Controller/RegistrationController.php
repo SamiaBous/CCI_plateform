e17dc6ba1d6b222f1e7bbc25ef8ba8ab250484c3
SamiaBous <samia.b@codeur.online>
2018-03-14 13:37:34 +0100

    registration

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,58 @@
+<?php
+// src/Controller/RegistrationController.php
+namespace App\Controller;
+use App\Form\UserType;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+class RegistrationController extends Controller
+{   private function Genere_Password($size)
+    {
+        $password = "";
+        // Initialisation des caractères utilisables
+        $characters = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+
+        for($i=0;$i<$size;$i++) //for (initialization; condition; increment-decrement){}The first part initializes the code. The second part is the condition that will continue to run the loop as long as it is true. The last part is what will be run after each iteration of the loop.
+        {
+            $password .= ($i%2) ? strtoupper($characters[array_rand($characters)]) : $characters[array_rand($characters)]; //$password contains ($i%2), strtoupper: renvoie une chaîne en majuscules, array_rand: prend une ou plusieurs valeurs au hasard dans un tableau.
+        }
+
+        return $password;
+    }
+
+
+    /**
+     * @Route("/register", name="user_registration")
+     */
+    public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        // 1) build the form
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        //dump($request);
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // 3) Encode the password (you could also do this via Doctrine listener)
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+            // 4) save the User!
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+            return $this->redirectToRoute('login');
+        }
+        return $this->render(
+            'inscription.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+}
+        
+?>
